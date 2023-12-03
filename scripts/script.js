edbf291f6e2526cd303a3cc9b6e92c1952790388
Marciapan <122381666+Marciapan@users.noreply.github.com>
2023-12-03 15:56:04 +0100

    HTML van beide zo goed als af
    Verder gaan met styling van laatste stukje Home en hele About. Also fixed the header being sticky/fixed!

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,18 @@
 console.log("hi");
 
 
+// FUCKING STICKY WILT NIET WERKEN WTF WAAROM NIET IK HAAT SCROLL EVENTS 
+
+var navBar = document.querySelector(".menu");
+var stickyNav = navBar.offsetTop;
+
+console.log(navBar);
+
+window.onscroll = function (stickyNav) {
+    if (scrollY > 0){
+        navBar.classList.add("sticky");
+    } else {
+        navBar.classList.remove("sticky");
+    }
+};
 
